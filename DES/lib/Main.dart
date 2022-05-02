7038ca853b8e75fa3cbef2e19345197271dc9fc9
unknown <greenzonecoc@gmail.com>
2022-05-02 08:45:42 +0300

    final

--- DES/lib/Main.dart
+++ DES/lib/Main.dart
@@ -1,11 +1,8 @@
 import 'DES.dart';
-import 'Utilities.dart';
 
 void main(List<String> args) {
   DES des = new DES();
   List<String> encyptedBlocks = des.encrypt("Hello World", "AABB09182736CCDD");
   print(encyptedBlocks);
-
-  // d1a87d37f5b6bfe1	51fcc0ce15d3de21
   print(des.getEncryptedText());
 }
